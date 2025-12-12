710d7da4d941e3e46c4e039e836f41f1bf7b5926
Ryotruuser <ryonantruuser@gmail.com>
2025-12-12 19:51:23 -0300

    css e estrutura dos elementos

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import logoImg from '/logo.png'
 
+function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <main className='container'>
+        <img 
+        className='logo'
+          src={logoImg} 
+          alt="Logo da calculadora da gasolina ou alcool" />
+
+        <h1 className='title'>Qual melhor opção?</h1>
+
+        <form className="form">
+          <label>Álcool (Preço por litro):</label>
+          <input 
+            className='input'
+            type="number" 
+            placeholder='4,90'
+            min="1"
+            step="0.01"
+            required
+            />
+
+          <label>Gasolina (Preço por litro):</label>
+          <input 
+            className='input'
+            type="number" 
+            placeholder='4,90'
+            min="1"
+            step="0.01"
+            required
+            />
+
+            <input type="submit" value="Calcular" className='btn' />
+        </form>
+
+      </main>
+      
+    </div>
   )
 }
 
